f00d47a18d7cd6084d486d21df242100f040a5de
unknown <nadjam@328-C4.nad.qc.ca>
2017-05-13 10:59:25 -0400

    Basic UI

---
+++ miniature_UI/Assets/UI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour {
+
+
+    private int food;
+    private int scrap;
+    private int science;
+
+    private bool upgradesOn = false;
+
+    public void Start() {
+        Debug.Log("Start of program");
+    }
+
+    public void showUpgrades() {
+            SceneManager.LoadScene("Upgrades", LoadSceneMode.Additive);
+
+
+    }
+}
